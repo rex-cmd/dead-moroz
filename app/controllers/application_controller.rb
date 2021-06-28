87648eb032e9618f296d1d8eaf60015d0b879949
rex-cmd <rexdouble9@gmail.com>
2021-06-29 02:48:41 +0300

    deploy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
   rescue_from CanCan::AccessDenied do
     redirect_back fallback_location: root_path, alert: 'Access denied!'
