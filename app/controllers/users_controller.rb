5c085dc9886479369c7142aa963f329784515abf
rex-cmd <rexdouble9@gmail.com>
2021-06-29 02:33:04 +0300

    translation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
     load_and_authorize_resource
     helper_method :user
-   
+    include GoogleTranslate 
     def index 
         @users = @users.kid.with_not_decided_gifts if can?(:toggle_selected, Gift) && user_params[:not_decided] == 'true'
         @users = @users.by_number_of_reviews if current_user.elf?
@@ -10,6 +10,15 @@ class UsersController < ApplicationController
 
     def show
         @estimate = user.estimates_on.find_by(author: current_user)
+
+        respond_to do |format|
+            format.js do
+              @translation = Rails.cache.fetch(['translate', user.cache_key_with_version], expires_in: 1.hour) do
+                GoogleTranslate.translate(@user.behavior)
+              end
+            end
+            format.html
+        end
     end
 
     private 
--- app/javascript/packs/users.js
+++ app/javascript/packs/users.js
@@ -1,28 +1,4 @@
-$(function(){
-  console.log('we are here')
-  $('#assessment-modal').on('hidden.bs.modal', function() {
-    $('#form-rating li.star').each(function(i) {
-      $(this).css('color', '#ccc');
-    });
-  });
-//stars
-    $('#form-rating li').on('mouseover', function() {
-        let onStar = parseInt($(this).data('value'), 10);
-        $(this).parent().children('li.star').each(function (i){
-          if (i < onStar) {
-            $(this).addClass('hover');
-          } else {
-            $(this).removeClass('hover');
-          }
-        });
-      });
-    
-      $('#form-rating li').on('mouseout', function(){
-        $('#form-rating li.star').each(function(){
-          $(this).removeClass('hover');
-        });
-      });
-    
+$(function(){   
       $('#form-rating li').on('click', function(){
         let onStar = parseInt($(this).data('value'), 10);
         $('#form-rating li.star').each(function(i) {
@@ -36,5 +12,8 @@ $(function(){
         $('#rating').val(onStar);
 
       });
-    
+      $('#translate').addEventListener('click',function() {
+        $(this).addClass('d-none');
+        $('#translation-spinner').removeClass('d-none');
+      });
 })
