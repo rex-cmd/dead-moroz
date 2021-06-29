87e54aa772447e426833545e2bf94fe4a6d72a4b
rex-cmd <rexdouble9@gmail.com>
2021-06-30 00:22:26 +0300

    hdh

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   host = 'https://obscure-sea-14271.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  config.action_mailer.default_charset = "utf-8"  
+  config.action_dispatch.default_charset = "utf-8"  
   config.action_mailer.perform_deliveries = true  
   config.action_mailer.delivery_method=:smtp
   config.action_mailer.smtp_settings={
