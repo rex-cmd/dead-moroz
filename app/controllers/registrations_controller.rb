25b8848e65eefba2d571075ef48f9771b009c1c1
Hannah <rexdouble9@gmail.coml>
2021-06-30 15:22:55 +0300

    asdf

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,3 @@
 class RegistrationsController < Devise::RegistrationsController
-      def create 
-        sign_up(resource_name, resource)
-        respond_with resource, location: after_sign_up_path_for(resource)
-      end
+    
 end
