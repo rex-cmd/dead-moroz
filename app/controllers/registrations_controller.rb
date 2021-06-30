0424cd42de971aeb276cb11187329bc7dc226014
Hannah <rexdouble9@gmail.coml>
2021-06-30 15:14:15 +0300

    custom registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,3 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
- 
-  end
+      def create 
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      end
+end
