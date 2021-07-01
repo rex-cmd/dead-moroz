32b10deb7b04f9d4e8f349862903c7103d05827d
Hannah <rexdouble9@gmail.coml>
2021-07-01 18:46:11 +0300

    another one

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,14 @@
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
+  config.storage = :fog
   config.fog_credentials = {
      :provider               => 'AWS',
-     :aws_access_key_id      => Rails.application.credentials.dig(:aws, :access_key_id),
-     :aws_secret_access_key  => Rails.application.credentials.dig(:aws, :secret_key_id),
-     :region                 => 'us-east-1',
-     :endpoint               => 'https://fra1.digitaloceanspaces.com'
+     :access_key_id          => Rails.application.credentials.dig(:spaces, :access_key_id),
+     :secret_access_key      => Rails.application.credentials.dig(:spaces, :secret_access_key),
+     :region                 => 'fra1',
+     :endpoint               => 'fra1.digitaloceanspaces.com'
   }
-  config.fog_directory  = 'https://fra1.digitaloceanspaces.com'
+  config.fog_directory  = Rails.application.credentials.dig(:spaces, :bucket)
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
   config.fog_public = false
-  config.asset_host = "https://fra1.digitaloceanspaces.com"
-  config.fog_attributes = { 'Cache-Control' => 'max-age=31557s6000' }
+  
 end
