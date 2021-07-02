19d218908e6d745b3cd068232ba98fb8ee658e81
Hannah <rexdouble9@gmail.coml>
2021-07-02 17:03:44 +0300

    0

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,19 +1,41 @@
-CarrierWave.configure do |config|
-  config.storage= :fog
-  # config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-     :provider               => 'AWS',
-     :aws_access_key_id      => Rails.application.credentials.spaces[:access_key_id],
-     :aws_secret_access_key  => Rails.application.credentials.spaces[:secret],
-     :region                 => 'nyc3',
-     :endpoint               => 'https://nyc3.digitaloceanspaces.com'
-  }
-  config.fog_directory  = 'dead-moroz2'
-  # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
-  # config.fog_public = false
-  #  config.asset_host = "https://dead-moroz2.nyc3.digitaloceanspaces.com"
-  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
-  # config.asset_host = "https://nyc3.digitaloceanspaces.com"
-  # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
-  # config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+if Rails.env.development?
+    CarrierWave.configure do |config|
+      config.storage= :fog
+      # config.fog_provider = 'fog/aws'
+      config.fog_credentials = {
+        :provider               => 'AWS',
+        :aws_access_key_id      => Rails.application.credentials.spaces[:access_key_id],
+        :aws_secret_access_key  => Rails.application.credentials.spaces[:secret],
+        :region                 => 'nyc3',
+        :endpoint               => 'https://nyc3.digitaloceanspaces.com'
+      }
+      config.fog_directory  = 'dead-moroz2'
+      # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
+      config.fog_public = false
+      #  config.asset_host = "https://dead-moroz2.nyc3.digitaloceanspaces.com"
+      config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
+      config.asset_host = "https://nyc3.digitaloceanspaces.com"
+      config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
+      # config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+    end
+else    
+  CarrierWave.configure do |config|
+    config.storage= :fog
+    # config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
+      :region                 => ENV['REGION'],
+      :endpoint               => ENV['ENDPOINT']
+    }
+    config.fog_directory  = ENV['SPACE_NAME']
+    # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
+    config.fog_public = false
+    #  config.asset_host = "https://dead-moroz2.nyc3.digitaloceanspaces.com"
+    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
+    config.asset_host = "https://nyc3.digitaloceanspaces.com"
+    config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
+    # config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+  end
 end
