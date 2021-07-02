ba24f47c8683f847d31e18c2a91425185c7cec7a
Hannah <rexdouble9@gmail.coml>
2021-07-02 16:11:33 +0300

    another one

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
      :provider               => 'AWS',
-     :aws_access_key_id      => Rails.application.credentials.dig(:spaces, :access_key_id),
-     :aws_secret_access_key  => Rails.application.credentials.dig(:spaces, :secret),
+     :aws_access_key_id      => Rails.application.credentials.spaces[:access_key_id],
+     :aws_secret_access_key  => Rails.application.credentials.spaces[:secret],
      :region                 => 'nyc3',
      :endpoint               => 'https://nyc3.digitaloceanspaces.com'
   }
